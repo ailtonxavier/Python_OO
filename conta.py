48cca335e5d4b48c217672862bd0291998b06e84
Ailton Xavier <ailton.xavier@hotmail.com>
2022-01-09 07:49:33 -0300

    Python OO aula 3

--- conta.py
+++ conta.py
@@ -1,7 +1,17 @@
 class Conta:
-    def __init__(self, numero, titular, saldo, limite):
-        print(f"Construindo objeto ... {self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+  # def __init__ é a função construtora, toda classe necessariamente precisa ter uma função init
+  def __init__(self, numero, titular, saldo, limite):
+    print(f"Construindo objeto ... {self}")
+    self.numero = numero
+    self.titular = titular
+    self.saldo = saldo
+    self.limite = limite
+
+  def extrato(self):
+    print(f"Saldo {self.saldo} do titular {self.titular}.")
+
+  def deposita(self, valor):
+    self.saldo += valor
+
+  def saque(self, valor):
+    self.saldo -= valor
